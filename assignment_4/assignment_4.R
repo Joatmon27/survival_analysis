50361f8d12d700e01fc991cea422385afbdd4db3
pwjvr <pwjansevanrensburg@capitecbank.co.za>
2019-08-28 11:40:43 +0200

    Finished up assignment 4

--- assignment_4/assignment_4.R
+++ assignment_4/assignment_4.R
@@ -1,14 +1,32 @@
+install.packages('installr')
+
+install.packages('survival')
+
 library(data.table)
 library(survival)
 library(survminer)
 library(ggfortify)
  
-data <- fread('Section1_9.dat',col.names = c('Surv_Time','Treatment','Censored'),colClasses = c('numeric', 'numeric', 'numeric'))
+data <- read.table('Section1_9.dat',col.names = c('Surv_Time','Treatment','Censored'),colClasses = c('numeric', 'numeric', 'numeric'))
 
 surv_obj <- survfit(Surv(time = data$Surv_Time, 
-                         event = data$Censored)~offset(exp(-0.045*data$Surv_Time)), 
-                    subset = data$Treatment==1,
+                         event = data$Censored)~data$Treatment, 
+                    subset = data$Treatment==2,
                     type="kaplan-meier")
+
+print(summary(surv_obj))
+
+surv.estimates <- surv_obj$surv
+surv.times <- surv_obj$time
+
+x <- seq(0, 50, length.out=1000)
+y <- exp(-0.045*x)
+
+plot(x=surv.times, y=surv.estimates, type='s', xlab="Time to infection", ylab="Survival estimate", 
+     main = "Leukemia free-survival times (in months) for Autologous and Allogeneic Transplants",
+     sub = "Based on Kaplan-Meier estimates", col = "blue", lty=2)
+lines(x = x,y, lwd=1.5, lty=1)
+legend(45,1,legend = c("Auto","Null Hypothesis"))
 
 ggsurv <- ggsurvplot(surv_obj , data = data, xlab="Time to infection",
                      ylab="Survival estimate",
